19abc9dd622f757afd03a753904339a353eff92c
Josh Bruegger <josh@bruegger.it>
2020-07-08 20:58:45 +0200

    Initial App commit

---
+++ argo_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ArgoApp());
+}
+
+class ArgoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ARgo',
+      home: Scaffold(),
+    );
+  }
+}
